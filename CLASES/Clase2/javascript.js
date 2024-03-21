83074ae7dc83462c84c4de753300200bbe055113
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2024-03-20 22:29:42 -0300

    js

---
+++ CLASES/Clase2/javascript.js
@@ -0,0 +1,79 @@
+/* 
+let 
+var
+const
+ */
+
+/* let nombre = "Nicolino";
+const apellido = "Locche";
+
+console.log(apellido);
+console.log(nombre);
+
+alert("Hola ingresá un numero");
+
+let num1 = Number( prompt("Ingresa un numero "));
+alert (num1)
+let num2 = Number (prompt("Ingresa un otro numero"));
+/*  */
+/* prompt por defecto es String */
+
+/* alert(num1 + num2);
+ */
+
+/* let num1 = Number( prompt("Ingresa un numero "))
+let num2 = Number (prompt("Ingresa un otro numero")); */
+
+/* function sumar(nombre, num1, num2) {
+  /* document.write(nombre+" el resultado de tu suma es: "+(num1+num2)) */
+
+/*   document.write(`${nombre} el resultado de tu suma es ${num1+num2}`);
+}
+sumar(prompt("decime tu nombre"),Number(prompt("Ingresa un numero ")),Number(prompt("Ingresa un otro numero"))
+); */
+
+/* funciones flechas */
+
+/* let restar = (num1,num2) =>{
+return num1-num2
+}
+ console.log(restar (10,5));  */
+/* 
+let restar = (num1, num2) => num1 - num2;
+console.log(restar(10, 5));
+
+let saludar = (nombre) => `Hola ${nombre}`;
+saludar("Agostina"); */
+
+/* 
+ condicional if */
+
+let edad = 18;
+
+/* if (edad >= 18) {
+  console.log("podes Pasar");
+} else {
+  console.log("NO podes Pasar");
+}
+ */
+/*  if ternario */
+/* condicion ? true : false */
+//evaluacion  ?true     :false
+/* edad >= 18 ? console.log("podes Pasar") : console.log("NO podes Pasar"); */
+
+/* CALLBACK */
+
+
+/* 
+let calculadora = (num1,num2,operacion) => operacion (num1,num2)
+let sumar = (num1, num2) => num1 + num2;
+let restar = (num1, num2) => num1 - num2;
+let multiplicar = (num1, num2) => num1 * num2;
+let dividir = (num1, num2) => num1 / num2;
+
+console.log(calculadora(10,10,sumar));
+console.log(calculadora(10,10,restar));
+console.log(calculadora(10,10,multiplicar));
+console.log(calculadora(10,10,dividir)); */
+
+
